5526a8e0ffa5040048dbd4e91ab5de1f331e2024
root <root@DESKTOP-FIQOQK1.localdomain>
2024-12-12 11:34:37 -0400

    Reverting changes

--- app/database/db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine, MetaData
-from databases import Database
-import os
-
-DATABASE_URL = 'postgresql://postgres:zanahoria12@localhost/movie_db'
-engine = create_engine(DATABASE_URL)
-metadata = MetaData()
-
-database = Database(DATABASE_URL)
--- app/main.py
+++ app/main.py
@@ -1,20 +1,7 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
 from app.routes.movies import movies
-from app.database.db import engine, metadata, database
-
-metadata.create_all(engine)
 
 app = FastAPI()
-
-@asynccontextmanager
-async def lifespan(app: FastAPI): 
-    
-    await database.connect()
-    
-    yield 
-    
-    await database.disconnect()
 
 app.include_router(movies)
 
